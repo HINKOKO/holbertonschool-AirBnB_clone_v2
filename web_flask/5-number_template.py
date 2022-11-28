e20a3abf5aafb853d265937920e8e43f54986435
root <piotr62@live.fr>
2022-11-28 01:40:18 -0800

    putting module according to jinja expression

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -51,12 +51,12 @@ def is_num(n):
 
 
 @app.route("/number_template/<int:n>", strict_slashes=False)
-def template(n):
+def numbersandtemp(n):
     """
     Displays 5-number.html template
     only if n is an integer
     """
-    return render_template("5-number.html", n=n)
+    return render_template("5-number.html", number=n)
 
 
 if __name__ == "__main__":
