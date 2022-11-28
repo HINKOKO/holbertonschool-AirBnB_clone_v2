535754530ea2c46df4425777fb0eaf6c0ad4dbb6
root <piotr62@live.fr>
2022-11-28 04:43:47 -0800

    fixing Session() to Session in reload() function

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -5,7 +5,7 @@ Database mode, to be used with SQLAlchemy
 """
 from os import getenv
 from sqlalchemy import create_engine, MetaData
-from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy.orm import Session, sessionmaker, scoped_session
 import models
 from models.amenity import Amenity
 from models.city import City
@@ -81,7 +81,7 @@ class DBStorage:
         session_factory = sessionmaker(
             bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(session_factory)
-        self.__session = Session()
+        self.__session = Session
 
     def close(self):
         """Call remove method on private session attribute"""
